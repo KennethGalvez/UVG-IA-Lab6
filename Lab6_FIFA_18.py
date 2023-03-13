4f30d82fd3c10791a766e774587a68c108dcb382
unknown <josemarianoreyesh02@gmail.com>
2023-03-13 17:59:33 -0600

    arreglo de comentarios

--- Lab6_FIFA_18.py
+++ Lab6_FIFA_18.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -10,10 +9,10 @@ from ydata_profiling import ProfileReport
 
 
 def compute_operation(cell):
-    # Use regular expressions to extract the operation
+    # Utiliza expresiones regulares para extraer la operación
     match = re.match(r"(\d+)\s*([+-])\s*(\d+)", str(cell))
     if match:
-        # If the cell contains an operation, compute the result and return it
+        # Si la celda contiene una operación, calcula el resultado y lo devuelve
         a = int(match.group(1))
         op = match.group(2)
         b = int(match.group(3))
@@ -22,11 +21,11 @@ def compute_operation(cell):
         else:
             return a - b
     else:
-        # If the cell does not contain an operation, return the original value
+        # Si la celda no contiene una operación, devuelve el valor original
         return cell
 
 
-# Load dataset
+# Cargar dataset
 data = pd.read_csv("CompleteDataset.csv", low_memory=False)
 
 # Generar un informe detallado de los datos
@@ -35,13 +34,13 @@ profile = ProfileReport(data, title="CompleteDataset", explorative=True)
 # Guardar el informe en un archivo HTML
 profile.to_file("CompleteDataset.html")
 
-# Apply compute_operation function to all columns
+# Aplicar la función compute_operation a todas las columnas
 data = data.applymap(compute_operation)
 
-# Drop rows with missing values
+# Eliminar las filas con valores faltantes
 data = data.dropna()
 
-# Drop unnecessary columns
+# Eliminar las columnas innecesarias
 data.drop(
     [
         "ID",
@@ -64,11 +63,11 @@ data.drop(
 
 data.to_csv("cleaned_data.csv", index=False)
 
-# Split dataset into training, validation, and test sets (80%, 10%, and 10% respectively)
+# Dividir el conjunto de datos en conjuntos de entrenamiento, validación y prueba 80%, 10%, 10%
 train, valtest = train_test_split(data, test_size=0.2, random_state=1)
 val, test = train_test_split(valtest, test_size=0.5, random_state=1)
 
-# Separate features and target variable
+# Separar las características y la variable objetivo
 train_x = train.drop(["Potential"], axis=1)
 train_y = train["Potential"]
 val_x = val.drop(["Potential"], axis=1)
@@ -76,28 +75,29 @@ val_y = val["Potential"]
 test_x = test.drop(["Potential"], axis=1)
 test_y = test["Potential"]
 
-# Train Decision Tree model
+# Entrenar el modelo de árbol de decisión
 model = DecisionTreeRegressor(random_state=1)
 model.fit(train_x, train_y)
 
-# Get feature importances and sort them in descending order
+# Obtener las importancias de las características y ordenarlas en orden descendente
 importances = model.feature_importances_
 indices = np.argsort(importances)[::-1]
 
-# Show top 5 features
+# Mostrar las 5 características principales
 print("Top 5 features:")
 for f in range(5):
-    print("%d. %s (%f)" % (f + 1, train_x.columns[indices[f]], importances[indices[f]]))
+    print("%d. %s (%f)" %
+          (f + 1, train_x.columns[indices[f]], importances[indices[f]]))
 
-# Evaluate the model on the validation set
+# Evaluar el modelo en el conjunto de validación
 y_val_pred = model.predict(val_x)
 print("R2 score on validation set: ", r2_score(val_y, y_val_pred))
 
-# Evaluate the model on the test set
+# Evaluar el modelo en el conjunto de prueba
 y_test_pred = model.predict(test_x)
 print("R2 score on test set: ", r2_score(test_y, y_test_pred))
 
-# Tune hyperparameters using Grid Search
+# Ajustar los hiperparámetros
 params = {
     "max_depth": [2, 3, 4, 5, 6, 7, 8, 9, 10],
     "min_samples_split": [2, 5, 10, 20],
@@ -109,9 +109,9 @@ grid_search.fit(pd.concat([train_x, val_x]), pd.concat([train_y, val_y]))
 
 print("Best hyperparameters: ", grid_search.best_params_)
 
-# Predict on test set using best model found by Grid Search
+# Predecir en el set de testing usando el mejor modelo encontrado con grid Search
 best_model = grid_search.best_estimator_
 y_test_pred = best_model.predict(test_x)
 
-# Calculate R2 score on test set
+# Clacular el puntaje R2 en el set de testing
 print("R2 score on test set using best model: ", r2_score(test_y, y_test_pred))
